f2442cae9c0bb538f36cab789b7a8c37de5e4b46
Larissa Barbosa <laracontato20@gmail.com>
2024-01-07 22:00:57 -0300

    creating project via api

---
+++ cypress/e2e/api/createProject.cy.js
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker'
+
+describe('Create Project', () => {
+    it('successfully', () => {
+      const project = {
+        name: `project-${faker.string.uuid()}`,
+        description: faker.string.sample(5)
+      }
+  
+      cy.api_createProject(project)
+        .then(response => {
+          expect(response.status).to.equal(201)
+          expect(response.body.name).to.equal(project.name)
+          expect(response.body.description).to.equal(project.description)
+        })
+    })
+  })
