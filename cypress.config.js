f78ec5d2708d9349a112d86692a7e1f67ae6fd57
Larissa Barbosa <laracontato20@gmail.com>
2024-01-13 23:52:18 -0300

    chore: testing a type of report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: false,
+  },
   fixturesFolder: false,
   video: false,
 })
+
